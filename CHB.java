60df6812283ce04596bc56bfcf91d470902e0921
Osmostrix <osmostrix@yahoo.com>
2016-11-05 20:30:27 -0700

    initial troubleshooting

--- CHB.java
+++ CHB.java
@@ -2,7 +2,16 @@
 // Chemical Bonding Simulator
 // November 5th, 2016
 
-public static void main () {
+import java.util.*;
+import java.io.*;
+
+public class CHB {
+	public static void main (String[] args) {
+	element hydrogen = new element(1,1,1);
+	}
 	
 }
+
+
+
 
---
+++ element.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+
+public class element {
+	
+	public int protons, electrons, neutrons;
+	
+	public element (int p, int e, int n) {
+		protons = p;
+		electrons = e;
+		neutrons = n;
+	}
+		
+}
